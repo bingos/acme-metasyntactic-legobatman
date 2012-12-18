ce3e46d72c4a8a2ed0810beaf6eb2f46d30b0655
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2012-12-18 10:28:26 +0000

    Kerpow!

---
+++ lib/Acme/MetaSyntactic/legobatman.pm
@@ -0,0 +1,98 @@
+package Acme::MetaSyntactic::legobatman;
+
+#ABSTRACT: LEGO Batman theme
+
+use strict;
+use warnings;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+
+q[You-Know-Who];
+
+=pod
+
+=head1 DESCRIPTION
+
+Characters from the LEGO Batman and LEGO Batman2 as gleaned from
+the L<http://www.gamefaqs.com/> walkthroughs.
+
+I<Kerpow!!>
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+Alfred
+Aquaman
+Bane
+Batgirl
+Batman
+Black_Canary
+Brainiac
+Bruce_Wayne
+Captain_Boomerang
+Catwoman
+Clark_Kent
+Clayface
+Clown_Goon
+Commissioner_Gordon
+Cyborg
+Dick_Grayson
+Fishmonger
+Freeze_Girl
+General_Zod
+Green_Lantern
+Harley_Quinn
+Hawkgirl
+Hawkman
+Huntress
+Hush
+Joker_Goon
+Joker_Henchman
+Killer_Croc
+Killer_Moth
+LexBot
+Lex_Luthor
+Lois_Lane
+Man_Bat
+Martian_Manhunter
+Military_Policeman
+Mime_Goon
+Mr_Freeze
+Nightwing
+Penguin_Goon
+Penguin_Henchman
+Penguin_Minion
+Poison_Ivy
+Poison_Ivy_Goon
+Police_Marksman
+Police_Officer
+Policeman
+Ras_al_Ghul
+Riddler_Goon
+Riddler_Henchman
+Robin
+SWAT
+Sailor
+Scientist
+Security_Guard
+Sinestro
+Supergirl
+Superman
+The_Flash
+The_Joker
+The_Mad_Hatter
+The_Penguin
+The_Riddler
+The_Scarecrow
+Two_Face
+Two_Face_Goon
+Vicki_Vale
+Wonder_Woman
+Yeti
+Zoo_Sweeper
